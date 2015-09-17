85773c67efdb9715619bb90041280734dab15228
Summer Brochtrup & Diana Holland <student@epicodus.com>
2015-09-17 10:29:03 -0700

    Add passing specs to test full functionality of makeDeck function

--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,14 @@ describe('makeDeck', function() {
     var deck = makeDeck();
     expect(deck).to.have.length(52);
   });
+
+  it("includes specific card", function() {
+    var deck = makeDeck();
+    expect(deck[0]).to.eql(["Clubs", "A"]);
+  });
+
+  it("includes specific card", function() {
+    var deck = makeDeck();
+    expect(deck[14]).to.eql(["Diamonds", "2"]);
+  });
 });
