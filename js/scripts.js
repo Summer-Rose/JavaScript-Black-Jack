5a8eaf8567c05909de7524809eb114eee73be04a
Summer Brochtrup & Diana Holland <student@epicodus.com>
2015-09-17 10:22:28 -0700

    Add function to create deck

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
+//TODO: generate deck via function
 
 
-$(document).ready(function() {
-  
-  });
-});
+function makeDeck() {
+    var deck = [];
+    var suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+    var ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                    "J", "Q", "K"];
+
+    for (var i = 0; i < suits.length; i++) {
+        for (var j = 0; j < ranks.length; j++) {
+          var card = [suits[i], ranks[j]];
+          deck.push(card);
+          debugger;
+        }
+    }
+    return deck;
+};
+
+//
+// $(document).ready(function() {
+//
+//   });
+// });
